8e82d1facfce21976d3031836a7393b96a7187b3
Luc <luc@aggett.com>
2024-09-23 01:00:49 +0200

    move log files from /var/log to project directory

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -2,6 +2,6 @@
 bind = '127.0.0.1:8080'
 worker_class = 'sync'
 loglevel = 'debug'
-accesslog = '/var/log/gunicorn/access_log_diytracker'
+accesslog = 'logs/access_log_diytracker'
 acceslogformat ="%(h)s %(l)s %(u)s %(t)s %(r)s %(s)s %(b)s %(f)s %(a)s"
-errorlog =  '/var/log/gunicorn/error_log_diytracker'
+errorlog =  'logs/error_log_diytracker'
