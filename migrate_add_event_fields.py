0658ee866f8c5fa5b2ba62b3b4a5e21425979fdd
Luc <luc@aggett.com>
2026-03-13 00:12:03 +0100

    add migration script

---
+++ migrate_add_event_fields.py
@@ -0,0 +1,8 @@
+from app import app, db
+
+with app.app_context():
+    with db.engine.connect() as conn:
+        conn.execute(db.text('ALTER TABLE event ADD COLUMN description TEXT'))
+        conn.execute(db.text('ALTER TABLE event ADD COLUMN source_url VARCHAR(300)'))
+        conn.commit()
+print('Migration complete')
