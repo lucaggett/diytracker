ca8b54ef1810dadf166a22d32d513ea88889b42f
Luc <luc@aggett.com>
2026-05-06 10:59:36 +0200

    add explanation for weird path code

--- migrations/migrate_add_auth_fields.py
+++ migrations/migrate_add_auth_fields.py
@@ -1,4 +1,5 @@
 import os, sys
+# add the parent diytracker directory to the path (fixes weird import errors)
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from app import app, db
