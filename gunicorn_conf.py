dcf7197cce3f5d3c760d29e89f8e519a3130d672
Luc <luc@aggett.com>
2026-05-06 22:54:25 +0200

    Update gunicorn_conf.py

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -1,6 +1,7 @@
 # /diytracker/gunicorn_conf.py
 bind = '127.0.0.1:8080'
 worker_class = 'sync'
+workers = 4
 loglevel = 'debug'
 accesslog = 'logs/access_log_diytracker'
 acceslogformat ="%(h)s %(l)s %(u)s %(t)s %(r)s %(s)s %(b)s %(f)s %(a)s"
